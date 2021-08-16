ad8cddcafaac5e212dab0ae76144564c365b7c1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-08-16 14:34:14 +0100

    added a delete button component

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import { FaTrash } from 'react-icons/fa';
+
+import { deleteStory } from '../services/storyService';
+import { useGlobalContext } from '../context/Context';
+import { DELETE_STORY } from '../context/types';
+import Button from './Button';
+
+const DeleteButton = ({ id }) => {
+    const { dispatch } = useGlobalContext();
+
+    const handleDelete = async (id) => {
+        try {
+            dispatch({
+                type: DELETE_STORY,
+                payload: id,
+            });
+            await deleteStory(id);
+            window.location.reload();
+        } catch (ex) {
+            if (ex.response && ex.response.status === 404)
+                toast.error('This story has already been deleted');
+        }
+    };
+
+    return (
+        <Button
+            text='Delete'
+            className='btn red'
+            icon={<FaTrash />}
+            onClick={() => handleDelete(id)}
+        />
+    );
+};
+
+export default DeleteButton;
