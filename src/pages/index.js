0df00911c8ceaabd9a74c7b0af968b8106605b9d
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-11-18 15:18:23 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import SinglePage from './SinglePage';
+import DashBoard from './DashBoard';
+import AddStory from './AddStory';
+import Register from './Register';
+import Account from './Account';
+import Stories from './Stories';
+import Update from './Update';
+import About from './About';
+import Error from './Error';
+import Login from './Login';
+import Home from './Home';
+
+export { About, Account, AddStory, DashBoard, Error, Home, Login, Register, SinglePage, Stories, Update };
