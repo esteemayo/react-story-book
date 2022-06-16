a60634c6e57a280b23aa3889a7cfce6623760ba2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-16 21:09:18 +0100

    first commit

---
+++ src/utils/LoadingToRedirect.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoadingToRedirect = () => {
+  const navigate = useNavigate();
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((currentCount) => --currentCount);
+    }, 1000);
+
+    count === 0 && navigate('/login');
+    return () => clearInterval(interval);
+  }, [count, navigate]);
+
+  return (
+    <div>
+      <h5>Redirecting you in {count} seconds</h5>
+    </div>
+  );
+};
+
+export default LoadingToRedirect;
