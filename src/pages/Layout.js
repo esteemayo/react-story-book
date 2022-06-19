10cfc0334036390cbc5b5839ad3e4e34a99922f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-20 00:18:55 +0100

    modified

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,14 +5,16 @@ import Alert from 'components/Alert';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 import AddButton from 'components/AddButton';
-import { useGlobalContext } from 'context/Context';
+import { useGlobalContext } from 'context/story/StoryContext';
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
 const Layout = () => {
-  const { user, alert } = useGlobalContext();
+  const { alert } = useGlobalContext();
+  const { user } = useGlobalAuthContext();
 
   return (
     <main>
