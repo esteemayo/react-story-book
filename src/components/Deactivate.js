5f901c0b0c25ad4ca3bc209a119d81ab83335009
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-20 00:04:25 +0100

    fixed a bug

--- src/components/Deactivate.js
+++ src/components/Deactivate.js
@@ -1,17 +1,24 @@
+import axios from 'axios';
 import { toast } from 'react-toastify';
 import { FaCogs } from 'react-icons/fa';
 
 import Title from './Title';
 import Button from './Button';
-import { useGlobalContext } from 'context/Context';
-import { deactivateAcc } from 'services/userService';
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { REACT_APP_DEV_API_URL, REACT_APP_PROD_API_URL } = process.env;
 
 const Deactivate = () => {
-  const { logout } = useGlobalContext();
+  const { logout } = useGlobalAuthContext();
 
   const deleteMe = async () => {
     try {
-      await deactivateAcc();
+      await axios.delete(
+        `${
+          devEnv ? REACT_APP_DEV_API_URL : REACT_APP_PROD_API_URL
+        }/users/delete-me`
+      );
       logout();
     } catch (ex) {
       console.error(ex.response.data.message);
