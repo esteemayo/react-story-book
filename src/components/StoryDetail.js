e3991190e67f0fa31bed284ab02aaa7d0fe4ab26
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-20 21:31:35 +0100

    implemented bookmark

--- src/components/StoryDetail.js
+++ src/components/StoryDetail.js
@@ -1,12 +1,57 @@
-import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
-import { FaPencilAlt } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { FaBookmark, FaRegBookmark, FaPencilAlt, FaEye } from 'react-icons/fa';
 
+import * as bookmarkAPI from 'services/bookmarkService';
+import * as actions from 'context/bookmark/BookMarkTypes';
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
+import { useGlobalBookmarkContext } from 'context/bookmark/BookMarkContext';
 
-const StoryDetail = ({ body, slug, title, author, createdAt }) => {
+const StoryDetail = ({ _id: id, body, slug, title, author, createdAt }) => {
   const { user } = useGlobalAuthContext();
+  const { bookmark, dispatch } = useGlobalBookmarkContext();
+
   const [readMore, setReadMore] = useState(false);
+
+  const handleSetAsBookmark = async () => {
+    try {
+      const { data } = await bookmarkAPI.createBookmark({ story: id });
+      dispatch({
+        type: actions.CREATE_BOOKMARK,
+        payload: data,
+      });
+      toast.success('Story set as bookmark');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnSetAsBookmark = async () => {
+    try {
+      await bookmarkAPI.deleteBookmark(bookmark?._id);
+      dispatch({ type: actions.DELETE_BOOKMARK });
+      toast.success('Story unset as bookmark');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const fetchBookmark = async () => {
+      try {
+        const { data } = await bookmarkAPI.getOneBookmark(id);
+        dispatch({
+          type: actions.FETCH_BOOKMARK,
+          payload: data,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    user && fetchBookmark();
+  }, [dispatch, id, user]);
 
   return (
     <>
@@ -26,6 +71,22 @@ const StoryDetail = ({ body, slug, title, author, createdAt }) => {
           <span className='card-title'>
             {new Date(createdAt).toDateString()}
           </span>
+          <div className='card-wrapper'>
+            {bookmark ? (
+              <FaBookmark
+                className='bookmark-icon'
+                onClick={handleUnSetAsBookmark}
+              />
+            ) : (
+              <FaRegBookmark
+                className='bookmark-icon'
+                onClick={handleSetAsBookmark}
+              />
+            )}
+            <span>
+              views <FaEye />
+            </span>
+          </div>
           <blockquote>
             {body &&
               `${readMore ? body : body.split(' ').splice(0, 50).join(' ')}...`}
