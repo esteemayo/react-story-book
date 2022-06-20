3761b3ea41d69060c0a0faf96f76abaa916a2828
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-20 21:37:25 +0100

    added BookmarkProvider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/auth/AuthContext';
 import { StoryProvider } from 'context/story/StoryContext';
+import { BookmarkProvider } from 'context/bookmark/BookMarkContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoryProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </StoryProvider>
+    <BookmarkProvider>
+      <StoryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </StoryProvider>
+    </BookmarkProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
