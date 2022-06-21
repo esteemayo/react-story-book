7ef7a38a7928fbcb517ad7ba223d3d46818fa2bc
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-21 15:40:14 +0100

    modified

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,19 +1,7 @@
-import { useLocation } from 'react-router-dom';
-
 const Error = () => {
-  const { pathname } = useLocation();
-
   return (
-    <div className='row'>
-      <div className='not-found'>
-        <h1>404</h1>
-        <h1>error</h1>
-        <h2>page not found</h2>
-        <h3>
-          the requested URL <span className='text-danger'>"{pathname}"</span>{' '}
-          was not found
-        </h3>
-      </div>
+    <div className='not-found'>
+      <img src='assets/img/404.png' alt='Not Found' />
     </div>
   );
 };
