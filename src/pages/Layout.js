fd95b380296a1f368f7a9ee0ee2957fe38107239
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-26 16:35:49 +0100

    modified

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,13 +5,13 @@ import Alert from 'components/Alert';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 import AddButton from 'components/AddButton';
+import ScrollToTop from 'components/ScrollToTop';
 import { useGlobalContext } from 'context/story/StoryContext';
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
 
 import 'react-toastify/dist/ReactToastify.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
-import ScrollToTop from 'components/ScrollToTop';
 
 const Layout = () => {
   const { alert } = useGlobalContext();
