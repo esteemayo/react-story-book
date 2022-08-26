ba4dac718399e4b8642dba820bb9429b3e5f4f41
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-26 15:12:47 +0100

    refactored

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,10 @@
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-import { searchStory } from 'services/storyService';
-import { useGlobalContext } from 'context/story/StoryContext';
+import Search from './Search';
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
-import { HIDE_LOADING, LOADING, SEARCH_STORY } from 'context/story/StoryTypes';
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const [search, setSearch] = useState('');
-
-  const { dispatch } = useGlobalContext();
   const { user, logout } = useGlobalAuthContext();
-
-  const handleSearch = async (e) => {
-    e.preventDefault();
-
-    if (search) {
-      dispatch({ type: LOADING });
-      try {
-        const { data } = await searchStory(search);
-        dispatch({
-          type: SEARCH_STORY,
-          payload: data,
-        });
-        dispatch({ type: HIDE_LOADING });
-        navigate(`/stories/search?q=${search}`);
-        setSearch('');
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      navigate('/stories');
-    }
-  };
 
   return (
     <nav>
@@ -114,19 +84,7 @@ const NavBar = () => {
           )}
 
           <li>
-            <form onSubmit={handleSearch}>
-              <div className='form-group'>
-                <input
-                  type='search'
-                  className='form-input'
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                />
-                <div className='search-icon-container'>
-                  <FaSearch className='search-icon' />
-                </div>
-              </div>
-            </form>
+            <Search />
           </li>
         </ul>
       </div>
