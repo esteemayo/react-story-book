66ab15c07788ce7ab8b47c2bdfb80be7bf40a2fd
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-13 01:56:03 +0100

    refactored

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,11 +3,10 @@ import { FaTrash } from 'react-icons/fa';
 
 import Button from './Button';
 import { deleteStory } from 'services/storyService';
-import { DELETE_STORY } from 'context/story/StoryTypes';
 import { useGlobalContext } from 'context/story/StoryContext';
 
 const DeleteButton = ({ id }) => {
-  const { dispatch } = useGlobalContext();
+  const { removeStory } = useGlobalContext();
 
   const handleDelete = async (id) => {
     if (window.confirm('Are you sure you want to delete this story')) {
@@ -17,10 +16,7 @@ const DeleteButton = ({ id }) => {
 
   const handleDeleteStory = async (id) => {
     try {
-      dispatch({
-        type: DELETE_STORY,
-        payload: id,
-      });
+      removeStory(id);
       await deleteStory(id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
