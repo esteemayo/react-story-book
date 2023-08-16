3926fd511c98826dc476e3bc9c5b6330cdbf4c4a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:23:46 +0100

    fix: change file extension to jsx

---
+++ src/components/Table.jsx
@@ -0,0 +1,13 @@
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
+
+const Table = ({ data, columns, onDelete }) => {
+  return (
+    <table className='striped'>
+      <TableHeader columns={columns} />
+      <TableBody data={data} onDelete={onDelete} />
+    </table>
+  );
+};
+
+export default Table;
