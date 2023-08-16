68f2cdefb310bf0caf54cb569ae4f8b50ee967c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 17:53:34 +0100

    fix: change file extension to jsx

---
+++ src/pages/Search.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from 'utils';
+import StoryCard from 'components/StoryCard';
+import { useGlobalContext } from 'context/story/StoryContext';
+
+const Search = () => {
+  const { stories } = useGlobalContext();
+
+  const query = useQuery();
+  const searchQuery = query.get('q');
+
+  if (stories.length === 0 && searchQuery) {
+    return (
+      <div className='container error-wrapper'>
+        <h1 className='story-error-msg'>
+          We couldn't find any matches for "{searchQuery}"
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <h1>Stories</h1>
+        {stories?.map((story) => {
+          return <StoryCard key={story._id} {...story} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
