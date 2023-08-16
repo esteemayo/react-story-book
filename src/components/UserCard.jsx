372e3424b7f4e8778e8e9cf75fe8c9a0c14f7d73
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:24:48 +0100

    fix: change file extension to jsx

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { REACT_APP_DEV_IMAGE_API_URL, REACT_APP_PROD_IMAGE_API_URL } =
+  process.env;
+
+const UserCard = ({ user }) => {
+  const PF = devEnv
+    ? REACT_APP_DEV_IMAGE_API_URL
+    : REACT_APP_PROD_IMAGE_API_URL;
+
+  return (
+    <div className='card center-align single-story-card'>
+      <div className='card-content'>
+        <span className='card-title'>{user?.name}</span>
+        <img
+          src={user?.photo ? PF + user.photo : user?.gravatar}
+          className='circle responsive-img'
+          alt=''
+        />
+      </div>
+      <div className='card-action'>
+        <Link to={`/stories?author=${user?.username}`}>
+          More From {user?.firstName}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
