7b84f04830bc6bf24f1a92a74f6730895cc013be
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:32:08 +0100

    fix: add useMemo react hook

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { createComment } from 'services/commentService';
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
