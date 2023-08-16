52585f820354a684d9e07d0677827081bc99af45
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:19:46 +0100

    fix: change file extension to jsx

---
+++ src/components/AccountSettings.jsx
@@ -0,0 +1,15 @@
+import UserData from './UserData';
+import Deactivate from './Deactivate';
+import UserPassword from './UserPassword';
+
+const AccountSettings = () => {
+  return (
+    <div className='col s6'>
+      <UserData />
+      <UserPassword />
+      <Deactivate />
+    </div>
+  );
+};
+
+export default AccountSettings;
