61d1c7baafba39f67a35d2f296c96a4cc6351805
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:25:23 +0100

    fix: change file extension to jsx

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,47 @@
+import Moment from 'react-moment';
+
+import Title from './Title';
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { REACT_APP_DEV_IMAGE_API_URL, REACT_APP_PROD_IMAGE_API_URL } =
+  process.env;
+
+const UserProfile = () => {
+  const { user } = useGlobalAuthContext();
+
+  const PF = devEnv
+    ? REACT_APP_DEV_IMAGE_API_URL
+    : REACT_APP_PROD_IMAGE_API_URL;
+
+  return (
+    <div className='col s6'>
+      <Title title='User profile' className='text-uppercase' />
+      <div className='thumbnail'>
+        <img
+          src={
+            user?.photo
+              ? PF + user.photo
+              : user.gravatar || user?.user?.photo
+              ? PF + user?.user?.photo
+              : user?.user?.gravatar
+          }
+          width={200}
+          alt='avatar'
+        />
+      </div>
+
+      <div className='user-info'>
+        <p>Name: {user.name}</p>
+        <p>
+          Email: <a href={`mailto:${user.email}`}>{user.email}</a>
+        </p>
+        <p>
+          Joined: <Moment format='MMMM YYYY'>{user.createdAt}</Moment>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
