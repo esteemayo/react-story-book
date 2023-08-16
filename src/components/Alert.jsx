06c5597ea6a04fa63672ee353ba647bb0a68593f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:20:12 +0100

    fix: change file extension to jsx

---
+++ src/components/Alert.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useGlobalContext } from 'context/story/StoryContext';
+
+const Alert = ({ msg, type }) => {
+  const { stories, hideAlert } = useGlobalContext();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      hideAlert();
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [stories, hideAlert]);
+
+  return (
+    <div className='container'>
+      <div className={`toast ${type}`}>{msg}</div>
+    </div>
+  );
+};
+
+export default Alert;
