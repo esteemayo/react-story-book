eaa67839d79ddefc10fe28cfd9d122cf55452359
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:20:00 +0100

    fix: change file extension to jsx

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
+
+const AddButton = () => {
+  return (
+    <div className='fixed-action-btn'>
+      <Link to='/stories/create' className='btn-floating btn-large red'>
+        <FaPlus />
+      </Link>
+    </div>
+  );
+};
+
+export default AddButton;
