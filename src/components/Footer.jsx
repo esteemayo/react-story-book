9d7ed20ba15e0208b57c51e61f8e924fb5648fe0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 17:15:22 +0100

    fix: refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  const year = new Date().getFullYear();
+  const year = useMemo(() => {
+    return new Date().getFullYear();
+  }, []);
 
   return (
     <footer className='page-footer grey darken-3'>
