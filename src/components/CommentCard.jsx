66f1f30467346d744cc40651ee97426b588da394
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 16:20:36 +0100

    fix: change file extension to jsx

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,30 @@
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { REACT_APP_DEV_IMAGE_API_URL, REACT_APP_PROD_IMAGE_API_URL } =
+  process.env;
+
+const CommentCard = ({ body, user, createdAt }) => {
+  const PF = devEnv
+    ? REACT_APP_DEV_IMAGE_API_URL
+    : REACT_APP_PROD_IMAGE_API_URL;
+
+  return (
+    <div className='card single-story-card'>
+      <div className='card-content'>
+        <p>{body}</p>
+        <div className='chip'>
+          <img src={user?.photo ? PF + user.photo : user.gravatar} alt='' />
+          <Link to={`/stories?author=${user.username}`}>{user.name}</Link>
+        </div>
+        <br />
+        <small>
+          Posted: <Moment format='MMMM Do YYYY'>{createdAt}</Moment>
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default CommentCard;
