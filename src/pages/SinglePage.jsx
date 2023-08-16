ec5ef9f335a808632a82cd2e85d53d33da7cb989
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-16 17:53:49 +0100

    fix: change file extension to jsx

---
+++ src/pages/SinglePage.jsx
@@ -0,0 +1,11 @@
+import SingleStory from 'components/SingleStory';
+
+const SinglePage = () => {
+  return (
+    <div className='container single-story'>
+      <SingleStory />
+    </div>
+  );
+};
+
+export default SinglePage;
