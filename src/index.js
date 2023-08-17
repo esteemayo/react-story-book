8753321670100b02dd3e38badb84ee099b228571
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 01:09:32 +0100

    fix: modify

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { StoryProvider } from 'context/story/StoryContext';
-import { AuthProvider } from 'context/auth/AuthContext';
-import { BookmarkProvider } from 'context/bookmark/BookMarkContext';
-import { HistoryProvider } from 'context/history/HistoryContext';
+import StoryProvider from 'context/story/StoryContext';
+import AuthProvider from 'context/auth/AuthContext';
+import BookmarkProvider from 'context/bookmark/BookMarkContext';
+import HistoryProvider from 'context/history/HistoryContext';
 
 ReactDOM.render(
   <HistoryProvider>
