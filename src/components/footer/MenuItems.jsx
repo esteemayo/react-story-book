b70059ab8bd908f54e6ab9acc329373369b0e8b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 18:00:27 +0100

    fix: modify

--- src/components/footer/MenuItems.jsx
+++ src/components/footer/MenuItems.jsx
@@ -2,10 +2,13 @@ import MenuItem from './MenuItem';
 
 const MenuItems = () => {
   return (
-    <ul>
-      <MenuItem path='/stories' label='Public Stories' />
-      <MenuItem path='/about' label='About' />
-    </ul>
+    <div className='col l4 offset-l2 s12'>
+      <h5 className='white-text'>Links</h5>
+      <ul>
+        <MenuItem path='/stories' label='Public Stories' />
+        <MenuItem path='/about' label='About' />
+      </ul>
+    </div>
   );
 };
 
