4e3f8b8d28b5a5f7d93dcf4581be38a0a20dca04
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 01:35:16 +0100

    feat: create useFavorite custom hook

---
+++ src/hooks/useFavorite.js
@@ -0,0 +1,26 @@
+import { useCallback, useMemo } from 'react';
+import { likeStory } from 'services/storyService';
+
+const useFavorite = ({ likes, user, actionId, onAction }) => {
+  const hasFavorited = useMemo(() => {
+    const userId = user.id ?? user?.user?._id;
+    const story = likes || [];
+    return story.includes(userId);
+  }, [likes, user]);
+
+  const toggleFavorite = useCallback(async () => {
+    try {
+      const { data: story } = await likeStory(actionId);
+      onAction(story);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [actionId, onAction]);
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
