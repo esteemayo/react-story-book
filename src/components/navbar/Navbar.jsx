1058a682751c17750ced1b5bb532da5795a2001b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 16:40:36 +0100

    fix: refactor

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,5 @@
-import { Link, NavLink } from 'react-router-dom';
-
 import Logo from './Logo';
-import Search from '../Search';
+import NavItems from './NavItems';
 
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
 
@@ -12,81 +10,10 @@ const Navbar = () => {
     <nav>
       <div className='nav-wrapper'>
         <Logo />
-        <ul id='nav-mobile' className='right hide-on-med-and-down'>
-          <li>
-            <NavLink
-              to='/stories'
-              className={({ isActive }) => `${isActive ? 'link active' : ''}`}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/about'
-              className={({ isActive }) => `${isActive ? 'link active' : ''}`}
-            >
-              About
-            </NavLink>
-          </li>
-          {user && (
-            <>
-              <li>
-                <NavLink
-                  to='/dashboard'
-                  className={({ isActive }) =>
-                    `${isActive ? 'link active' : ''}`
-                  }
-                >
-                  Welcome {user?.firstName || user?.user?.firstName}
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/account'
-                  className={({ isActive }) =>
-                    `${isActive ? 'link active' : ''}`
-                  }
-                >
-                  Account
-                </NavLink>
-              </li>
-              <li>
-                <Link to='#' onClick={logout}>
-                  Logout
-                </Link>
-              </li>
-            </>
-          )}
-          {!user && (
-            <>
-              <li>
-                <NavLink
-                  to='/login'
-                  className={({ isActive }) =>
-                    `${isActive ? 'link active' : ''}`
-                  }
-                >
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/register'
-                  className={({ isActive }) =>
-                    `${isActive ? 'link active' : ''}`
-                  }
-                >
-                  Register
-                </NavLink>
-              </li>
-            </>
-          )}
-
-          <li>
-            <Search />
-          </li>
-        </ul>
+        <NavItems
+          user={user}
+          logout={logout}
+        />
       </div>
     </nav>
   );
