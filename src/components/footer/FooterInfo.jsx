55c71faeebbce5fc03ae2ed8145addc10c59f275
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 18:06:32 +0100

    feat: create FooterInfo component

---
+++ src/components/footer/FooterInfo.jsx
@@ -0,0 +1,12 @@
+const FooterInfo = () => {
+  return (
+    <div className='col l6 s12'>
+      <h5 className='white-text'>StoryBooks</h5>
+      <p className='grey-text text-lighten-4'>
+        Share your life with the world
+      </p>
+    </div>
+  );
+};
+
+export default FooterInfo;
