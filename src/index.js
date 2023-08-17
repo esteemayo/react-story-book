a25bdd9cca4002e81d4ae84709a839ce30a0122b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 17:18:58 +0100

    fix: add css file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import AuthProvider from 'context/auth/AuthContext';
 import BookmarkProvider from 'context/bookmark/BookMarkContext';
 import HistoryProvider from 'context/history/HistoryContext';
 
+import './index.css';
+
 ReactDOM.render(
   <HistoryProvider>
     <BookmarkProvider>
