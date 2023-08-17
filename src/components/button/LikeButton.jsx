d1167696d5330bd50af59ccdb350b77dd7568960
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 17:42:52 +0100

    fix: modify

--- src/components/button/LikeButton.jsx
+++ src/components/button/LikeButton.jsx
@@ -12,7 +12,7 @@ const LikeButton = ({ likes, user, actionId }) => {
 
   const handleLike = useCallback(async (e) => {
     e.stopPropagation();
-    return !!user ? null : toggleFavorite();
+    return !user ? null : toggleFavorite();
   }, [user, toggleFavorite]);
 
   return (
