96cfa654f7c65d5fc011a84489b96133a55bbb43
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 15:15:11 +0100

    fix: import prop-types module

--- src/components/comment/CommentCard.jsx
+++ src/components/comment/CommentCard.jsx
@@ -1,6 +1,7 @@
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import { useMemo } from 'react';
+import PropTypes from 'prop-types';
 
 const devEnv = process.env.NODE_ENV !== 'production';
 const { REACT_APP_DEV_IMAGE_API_URL, REACT_APP_PROD_IMAGE_API_URL } =
