b8114e18cae7f49f461bdab61016a3098339493e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-17 16:36:43 +0100

    feat: create Logo component

---
+++ src/components/navbar/Logo.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+  return (
+    <Link to='/' className='brand-logo'>
+      StoryBooks
+    </Link>
+  );
+};
+
+export default Logo;
