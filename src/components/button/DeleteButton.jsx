d01b4e8e550c3bab775ed352946d03be46e80b99
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-01 02:45:28 +0100

    fix: refactor

--- src/components/button/DeleteButton.jsx
+++ src/components/button/DeleteButton.jsx
@@ -3,37 +3,16 @@ import { useCallback } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
-import { deleteStory } from 'services/storyService';
-import { useGlobalContext } from 'context/story/StoryContext';
-
 import Button from './Button';
 
-const DeleteButton = ({ onAction }) => {
-  const { removeStory } = useGlobalContext();
-
-  const handleDeleteStory = useCallback(async (id) => {
-    try {
-      removeStory(id);
-      await deleteStory(id);
-    } catch (ex) {
-      if (ex.response && ex.response.status === 404)
-        toast.error('This story has already been deleted');
-    }
-  }, [removeStory]);
-
-  const handleDelete = useCallback(async (id) => {
-    if (window.confirm('Are you sure you want to delete this story')) {
-      await handleDeleteStory(id);
-    }
-  }, [handleDeleteStory]);
-
+const DeleteButton = ({ actionId, onAction }) => {
   return (
     <Button
       type='button'
       text='Delete'
       className='btn red'
       icon={<FaTrash />}
-      onClick={() => handleDelete(id)}
+      onClick={() => onAction(actionId)}
     />
   );
 };
