1d7992740ab5d1376b78d61ff8764b3350d47d0a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-01 03:30:30 +0100

    fix: modify

--- src/components/account/Deactivate.jsx
+++ src/components/account/Deactivate.jsx
@@ -27,8 +27,8 @@ const Deactivate = ({ swal }) => {
       text: 'You won\'t be able to revert this!',
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      confirmButtonColor: '#26a69a',
+      cancelButtonColor: '#f44336',
       confirmButtonText: 'Yes, delete it!'
     }).then(async (result) => {
       if (result.isConfirmed) {
